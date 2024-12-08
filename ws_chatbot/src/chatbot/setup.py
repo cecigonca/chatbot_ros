54d86f306ec2251533e855a62643d6846d8906b2
cecilia <cecilia.goncalves@sou.inteli.edu.br>
2024-12-08 18:07:48 -0300

    atualização config e ambiente

--- ws_chatbot/src/chatbot/setup.py
+++ ws_chatbot/src/chatbot/setup.py
@@ -4,19 +4,19 @@ package_name = 'chatbot'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'nltk',],
     zip_safe=True,
     maintainer='celia123',
     maintainer_email='cecilia.goncalves@sou.inteli.edu.br',
     description='Chatbot para controle de robôs no ROS 2',
-    license='TODO: License declaration',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
